a7e91ce076eb4e0dd8ac56ed5e92c67ed05a3596
Abbas Haider <abbas.haider1058@gmail.com>
2025-04-18 17:25:17 +0500

    testing

--- src/addition.py
+++ src/addition.py
@@ -4,6 +4,6 @@ def add(a, b):
     return a + b
 
 def test_add():
-    assert add(1, 2) == 3, "The sum of 1 and 2 should be 3"
+    assert add(1, 2) == 3, "The sum of 1 and 2 should be s3"
     assert add(1, -1) == 0, "The sum of 1 and -1 should be 0"
 
